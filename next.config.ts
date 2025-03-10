576805347c26b5b0f787ce5d968f5888473c6a1d
William Astudillo <william.astudillo@pragma.com.co>
2025-03-09 20:06:51 -0500

    feature: add static files and initial config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+import { variables } from "@app/sass/variables";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    additionalData: variables,
+  },
+  images: {
+    domains: ["media.es.wired.com"],
+  },
 };
 
 export default nextConfig;
