89da14ce342898c9c50fa741ffbc3af663636022
William Astudillo <william.astudillo@pragma.com.co>
2025-03-09 21:12:47 -0500

    refactor: add refactor in global variable

--- src/app/sass/variables.ts
+++ src/app/sass/variables.ts
@@ -2,6 +2,7 @@ export const variables = `
     $color-yellow: #ffe600
     $color-backgorund: #ededed
     $color-white: #fff
-    $color-text: #CFCFCF
+    $color-text1: #CFCFCF
+    $color-text2: rgba(0, 0, 0, .55)
     $border-radius: 4px
 `;
