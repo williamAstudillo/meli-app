bef84bf396c6d681b120a94ff93835e38a121846
William Astudillo <william.astudillo@pragma.com.co>
2025-03-12 12:49:59 -0500

    refactor: refactor Home page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
-import Card from "@app/components/atoms/card/Card";
+"use client";
+import BreadCrumb from "@src/components/atoms/breadCrumb/BreadCrumb";
+import Card from "@src/components/atoms/card/Card";
+import useGetProducts from "@src/hooks/useGetProducts";
+import useStore from "@src/store/useStore";
+import Link from "next/link";
 
 export default function Home() {
-  const URL =
-    "https://media.es.wired.com/photos/648c9a1ba566376ee967bf6a/master/pass/459293406";
-  const title =
-    "Consola Microsoft Xbox Series X Digital 1tb Robot White. Color Blanco";
+  const { query } = useStore();
+  const products = useGetProducts(query);
   return (
     <>
-      <Card city="Bogota" price={1980} title={title} url={URL} />
-      <Card city="Bogota" price={1980} title={title} url={URL} />
-      <Card city="Bogota" price={1980} title={title} url={URL} />
-      <Card city="Bogota" price={1980} title={title} url={URL} />
+      <BreadCrumb categories={[]} />
+      {products.map((product) => (
+        <Link href={`/items/${product.id}`} key={product.id}>
+          <Card
+            key={product.id}
+            price={product.price}
+            title={product.title}
+            url={product.picture}
+          />
+        </Link>
+      ))}
     </>
   );
 }
