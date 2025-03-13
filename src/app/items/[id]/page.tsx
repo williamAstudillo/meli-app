3185602e97d140502668e21fd381473397629a84
William Astudillo <william.astudillo@pragma.com.co>
2025-03-12 22:14:40 -0500

    feat: add SEO to dinamyc and static pages

--- src/app/items/[id]/page.tsx
+++ src/app/items/[id]/page.tsx
@@ -6,6 +6,27 @@ import ItemDetail from "@src/components/screens/ItemDetail/ItemDetail";
 import { getProductsRepository } from "@src/modules/infraestructure/getProductsRepository";
 import BreadCrumb from "@src/components/atoms/breadCrumb/BreadCrumb";
 
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const productDetail = await getProductDetail(getProductsRepository(), id);
+  return {
+    title: productDetail?.title,
+    description: productDetail?.title,
+    image: productDetail?.picture,
+    openGraph: {
+      title: productDetail?.title,
+      description: productDetail?.title,
+      image: productDetail?.picture,
+      type: "website",
+      url: `https://localhost:3000/items/${id}`,
+    },
+  };
+}
+
 const ItemDetailPage = async ({
   params,
 }: {
